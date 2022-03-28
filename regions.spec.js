17ba85621993c6e4eab795548ed015827c5a4b61
Saurabh Prakash <saurabh_prakash@yahoo.com>
2022-03-28 08:16:18 +0530

    Failing a spec

--- regions.spec.js
+++ regions.spec.js
@@ -6,7 +6,7 @@ var chinaRegions = require(__dirname + '/helpers/aws/regions_china.js');
 describe('AWS Regions', function() {
     it('should have same regions for AWS, China, and GovCloud', function() {
         Object.keys(regRegions).forEach(function(regRegion){
-            expect(govRegions[regRegion], `AWS Region: ${regRegion} is not present in GovCloud region list`).to.be.an('array');
+            expect(govRegions[regRegion], `AWS 1 Region: ${regRegion} is not present in GovCloud region list`).to.be.an('array');
             expect(chinaRegions[regRegion], `AWS Region: ${regRegion} is not present in China region list`).to.be.an('array');
         });
     });
